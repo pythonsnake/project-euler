f5ecfa19c44bbc1ff8dd4f08c4de27f31f48e8d8
Python Snake <pythonsnake@HP-G62.(none)>
2011-10-08 00:35:14 +0100

    First commit
    	new file:   getProblems.py

---
+++ getProblems.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Get the problems and store them in problem{number}.py as Docstrings
+"""
+import sys
+import urllib
+import lxml
+import lxml.html
+
+min=int(sys.argv[1])
+max=int(sys.argv[2])
+
+while max>=min:
+	source = urllib.urlopen('http://projecteuler.net/problem={0}'.format(str(min))).read()
+	tree = lxml.html.document_fromstring(source)
+	problem = tree.xpath("//div[@class='problem_content']")[0].text_content().strip().capitalize()
+	name = "problem{0}.py".format(min)
+
+	with open(name, "w") as x:
+		x.write('"""\n{0}\n"""'.replace(u'\xa0',u'').format(problem))
+	min+=1
