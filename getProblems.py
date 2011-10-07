7ca44b1229f53863fd8f0b2d1ea8d60380ac5681
Python Snake <pythonsnake98@gmail.com>
2011-10-08 00:51:53 +0100

    Done

--- getProblems.py
+++ getProblems.py
@@ -10,12 +10,13 @@ import lxml.html
 min=int(sys.argv[1])
 max=int(sys.argv[2])
 
+if min>max: print "No"
 while max>=min:
 	source = urllib.urlopen('http://projecteuler.net/problem={0}'.format(str(min))).read()
 	tree = lxml.html.document_fromstring(source)
-	problem = tree.xpath("//div[@class='problem_content']")[0].text_content().strip().capitalize()
+	problem = tree.xpath("//div[@class='problem_content']")[0].text_content().strip().capitalize().encode("utf-8")
 	name = "problem{0}.py".format(min)
 
 	with open(name, "w") as x:
-		x.write('"""\n{0}\n"""'.replace(u'\xa0',u'').format(problem))
+		x.write('"""\n{0}\n"""'.format(problem))
 	min+=1
